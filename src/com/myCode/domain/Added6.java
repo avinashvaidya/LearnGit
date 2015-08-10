0e0b66b17ed5c8b4134aba0d0a4a632ed5da88f1
pankajgoyal87 <pankaj.r.goyal87@gmail.com>
2015-08-10 16:46:51 -0400

    I am done

--- src/com/myCode/domain/Added6.java
+++ src/com/myCode/domain/Added6.java
@@ -10,10 +10,6 @@ package com.myCode.domain;
 public class Added6 {
 	
 
-<<<<<<< HEAD
 	private String test = "what the hell is this 1st conflict. 123";
-=======
-	private String test = "what the hell is this 1st conflict 234.";
->>>>>>> branch 'masterGit' of https://github.com/avinashvaidya/LearnGit.git
 
 }
